44cd531f573693504c8739ed4a4c222fa256ea21
Thomas Mathew, Nikky (US - Mumbai) <nikky17may@gmail.com>
2020-10-04 11:58:42 +0530

    Updates

---
+++ precache-manifest.30356a0c3d7173d035f642f6ce674535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e44d601ea9daf5490fc9f335bb08c5",
+    "url": "/OHLC-Chart-React/index.html"
+  },
+  {
+    "revision": "ca3935518ab5c287f9e4",
+    "url": "/OHLC-Chart-React/static/css/main.724c7d4f.chunk.css"
+  },
+  {
+    "revision": "e9321a270446ecd9f230",
+    "url": "/OHLC-Chart-React/static/js/2.23dad2d4.chunk.js"
+  },
+  {
+    "revision": "2ae68f854965c8a762d6ef57a9f1ea1c",
+    "url": "/OHLC-Chart-React/static/js/2.23dad2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3935518ab5c287f9e4",
+    "url": "/OHLC-Chart-React/static/js/main.33464a98.chunk.js"
+  },
+  {
+    "revision": "60ea372d74c02f8567a9",
+    "url": "/OHLC-Chart-React/static/js/runtime-main.11950f0b.js"
+  }
+]);
